da156bf47692603bb1bdd070e41375b4d6159ae9
Rui Shi <shirui1062979473@gmail.com>
2025-10-08 13:30:04 -0500

    10/7/2025

--- Linked Lists/Basic/147_Insertion Sort List_M.py
+++ Linked Lists/Basic/147_Insertion Sort List_M.py
@@ -34,6 +34,95 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        Sorts a singly linked list using the insertion sort algorithm.
         
+        Args:
+            head: The head of the singly linked list.
+            
+        Returns:
+            The head of the sorted singly linked list.
+        """
+        
+        # Base case: if the list is empty or has only one node, it's already sorted.
+        if not head or not head.next:
+            return head
+        
+        # Create a dummy node to serve as the start of the sorted list.
+        # This simplifies inserting nodes at the beginning of the list.
+        dummy = ListNode(0, head)
+        
+        # `last_sorted` points to the last node in the sorted part of the list.
+        # `current` is the node we are considering inserting into the sorted part.
+        last_sorted = head
+        current = head.next
+        
+        while current:
+            if current.val >= last_sorted.val:
+                # If the current node is already in the correct order, just move on.
+                last_sorted = last_sorted.next
+            else:
+                # The current node is smaller and needs to be moved.
+                # Find the correct insertion point starting from the beginning.
+                prev = dummy
+                while prev.next.val <= current.val:
+                    prev = prev.next
+                
+                # Unlink `current` from its original position.
+                last_sorted.next = current.next
+                
+                # Insert `current` into its new correct position.
+                current.next = prev.next
+                prev.next = current
+            
+            # Move to the next node in the original list to be sorted.
+            current = last_sorted.next
+            
+        return dummy.next
+
+
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # base case, if the list has only one node or less
+        
+        if not head or not head.next:
+            return head
+        
+        dummy = ListNode(0, head)
+        
+        last_sorted = head # the last node that is sorted
+        cur = head.next
+        
+        while cur:
+            if cur.val >= last_sorted.val: # the current node is already sorted
+                last_sorted = cur
+                
+            else:
+                pointer = dummy
+                while cur.val > pointer.next.val:
+                    pointer = pointer.next
+                
+                # unlink the current node
+                last_sorted.next = cur.next
+                
+                # insert to new position
+                cur.next = pointer.next
+                pointer.next = cur
+                
+            cur = last_sorted.next
+        
+        return dummy.next
+            
+            
+            
